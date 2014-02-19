1753873d381e545c3ef9014bb07c40339083da5b
Eric John Adamos <netzon.eric@gmail.com>
2014-02-19 08:32:51 +0800

    Feedback list cell: Create preliminary implementation

---
+++ Insight/SSFeedbackListCell.h
@@ -0,0 +1,13 @@
+//
+//  SSFeedbackListCell.h
+//  Insight
+//
+//  Created by Eric John Adamos on 2/19/14.
+//  Copyright (c) 2014 Eric John Adamos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSFeedbackListCell : UITableViewCell
+
+@end
---
+++ Insight/SSFeedbackListCell.m
@@ -0,0 +1,34 @@
+//
+//  SSFeedbackListCell.m
+//  Insight
+//
+//  Created by Eric John Adamos on 2/19/14.
+//  Copyright (c) 2014 Eric John Adamos. All rights reserved.
+//
+
+#import "SSFeedbackListCell.h"
+
+@implementation SSFeedbackListCell
+
+- (id) initWithStyle: (UITableViewCellStyle) style
+     reuseIdentifier: (NSString*) reuseIdentifier
+{
+  self = [super initWithStyle: style
+              reuseIdentifier: reuseIdentifier];
+  
+  if (self != nil)
+  {
+
+  }
+  
+  return self;
+}
+
+- (void) setSelected: (BOOL) selected
+            animated: (BOOL) animated
+{
+  [super setSelected: selected
+            animated: animated];
+}
+
+@end
